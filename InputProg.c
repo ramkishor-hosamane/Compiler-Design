9373479f8c3ca2038158000c5118fd3530fe5f34
Vinutna Kolachana <vinnukolachana@gmail.com>
2020-12-02 17:47:49 +0530

    Vinutna's Entries to the repo... InputProg.c to give input C program... Vin_Token.py for token generation

---
+++ InputProg.c
@@ -0,0 +1,11 @@
+// Made for Lexical Analysis
+/* Program as Input for LabAssignent3 */
+/**/
+#include <stdio.h> // This is a header file
+int main()
+{
+    int a;
+    a = 10;
+    printf("The value of a is %d ",a);
+    return 0;
+}
